a75a1e88f68e70413faf63e5cd46a7a4f62147b9
junho <jh102328@khu.ac.kr>
2024-10-30 22:54:27 +0900

    [LS-4] 외부 api 연동 - 네이버 응답 형식 적용

--- external/naver-client/src/main/java/com/library/feign/NaverClient.java
+++ external/naver-client/src/main/java/com/library/feign/NaverClient.java
@@ -1,13 +1,15 @@
 package com.library.feign;
 
+import com.library.NaverBookResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "naverClient", url="${external.naver.url}", configuration = NaverClientConfiguration.class)
 public interface NaverClient {
+
     @GetMapping("/v1/search/book.json")
-    String search(@RequestParam("query") String query,
-                  @RequestParam("start") int start,
-                  @RequestParam("display") int display);
+    NaverBookResponse  search(@RequestParam("query") String query,
+                             @RequestParam("start") int start,
+                             @RequestParam("display") int display);
 }

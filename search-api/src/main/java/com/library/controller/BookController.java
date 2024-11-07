a4be15d8e141a42ddd7b1369e6de3d08bfb06d33
junho <jh102328@khu.ac.kr>
2024-11-08 03:55:48 +0900

    [LS-8] 문서화 적용

--- search-api/src/main/java/com/library/controller/BookController.java
+++ search-api/src/main/java/com/library/controller/BookController.java
@@ -1,12 +1,21 @@
 package com.library.controller;
 
 import com.library.controller.request.SearchRequest;
+import com.library.controller.response.ErrorResponse;
 import com.library.controller.response.PageResult;
 import com.library.controller.response.SearchResponse;
 import com.library.controller.response.StatResponse;
 import com.library.service.BookApplicationService;
 import com.library.service.BookQueryService;
 import com.library.service.DailyStatQueryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,21 +31,34 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/books")
 @Slf4j
+@Tag(name = "book", description = "book API")
 public class BookController {
     private final BookApplicationService bookApplicationService;
 
+    @Operation(summary = "search API", description = "도서 검색결과 제공", tags = { "book" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = PageResult.class))),
+            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))})
     @GetMapping
     public PageResult<SearchResponse> search(@Valid SearchRequest request){
         log.info("[BookController] search={}", request);
         return bookApplicationService.search(request.getQuery(), request.getStart(), request.getDisplay());
     }
 
+    @Operation(summary = "stats API", description = "쿼리 통계결과 제공", tags = { "book" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = StatResponse.class))),
+            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))})
     @GetMapping("/stats")
     public StatResponse findQueryStats(@RequestParam("query") String query, @RequestParam("date") LocalDate date){
         log.info("[BookController] find stats query={}, date={}", query, date);
         return bookApplicationService.findQueryCount(query, date);
     }
 
+    @Operation(summary = "stats ranking API", description = "조회수 상위 쿼리 통계결과 제공 ", tags = {"book"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content(array = @ArraySchema(schema = @Schema(implementation = StatResponse.class)))),
+            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))})
     @GetMapping("/stats/ranking")
     public List<StatResponse> findStatRanking(){
         log.info("[BookController] find top 5 stats");
--- search-api/src/main/java/com/library/controller/request/SearchRequest.java
+++ search-api/src/main/java/com/library/controller/request/SearchRequest.java
@@ -1,30 +1,30 @@
 package com.library.controller.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
-@ToString
 @Setter
+@ToString
+@Schema(description = "검색 요청을 위한 모델")
 public class SearchRequest {
-
-    // 50자
     @NotBlank(message = "입력은 비어있을 수 없습니다.")
     @Size(max = 50, message = "query는 최대 50자를 초과할 수 없습니다.")
+    @Schema(description = "검색쿼리", example = "HTTP", requiredMode = Schema.RequiredMode.REQUIRED, maxLength = 50)
     private String query;
 
+    @NotNull(message = "페이지 번호는 필수입니다.")
+    @Min(value = 1, message = "페이지번호는 1이상이어야 합니다.")
+    @Max(value = 10000, message = "페이지번호는 10000이하여야 합니다.")
+    @Schema(description = "페이지 번호", example = "1", requiredMode = Schema.RequiredMode.REQUIRED, minLength = 1, maxLength = 10000)
+    private Integer start;
 
-    // 1 ~ 10000
-    @NotNull(message = "입력은 비어있을 수 없습니다.")
-    @Min(value = 1, message = "페이지 번호는 1 이상이어야 합니다.")
-    @Max(value = 10000, message = "페이지 번호는 10000 이하여야 합니다.")
-    private int start;
-
-    // 1 ~ 50
-    @NotNull(message = "입력은 비어있을 수 없습니다.")
-    @Min(value = 1, message = "페이지 크기는 1 이상이어야 합니다.")
-    @Max(value = 50, message = "페이지 크기는 50 이하여야 합니다.")
+    @NotNull(message = "페이지 사이즈는 필수입니다.")
+    @Min(value = 1, message = "페이지크기는 1이상이어야 합니다.")
+    @Max(value = 50, message = "페이지크기는 50이하여야 합니다.")
+    @Schema(description = "페이지 사이즈", example = "10", requiredMode = Schema.RequiredMode.REQUIRED, minLength = 1, maxLength = 50)
     private int display;
 }
